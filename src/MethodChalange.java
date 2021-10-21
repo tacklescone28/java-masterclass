4dc4a50d059ed9e5e61cec00d26035406cc2b932
tacklescone28 <92543069+tacklescone28@users.noreply.github.com>
2021-10-21 18:31:38 +0200

    zadania od 50 d0 55

---
+++ src/MethodChalange.java
@@ -0,0 +1,24 @@
+public class MethodChalange {
+    public static void main(String[] args) {
+        checkNumber(-10);
+        checkNumber(5);
+        checkNumber(0);
+    }
+
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else if (number == 0) {
+            System.out.println("zero");
+        }
+    }
+
+    public static long toMilesPerH(double kmph) {
+        if(kmph < 0){
+            return -1;
+        }
+        return Math.round(kmph / 1.609);
+    }
+}
